5ab7ce2fa0a36822eab87f2cf1c22268fb16a3e7
C Dharmateja <cdharmateja1999@gmail.com>
2023-03-08 08:15:21 +0530

    fix

--- docs/build.sh
+++ docs/build.sh
@@ -1,1 +1,1 @@
-(go build -ldflags="-s -w" -v -o /go/bin/autodelete /go/src/app/cmd/autodelete); cp /go/bin/autodelete ./autodelete
+(go build -buildvcs=false -ldflags="-s -w" -v -o /go/bin/autodelete /go/src/app/cmd/autodelete); cp /go/bin/autodelete ./autodelete
